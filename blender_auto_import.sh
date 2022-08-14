6843af6e298907fe3b6606b8a6b34f73576de08e
TheKenetics <TheKenetics@users.noreply.github.com>
2022-08-13 19:10:00 -0500

    Fix bash environment var not working

--- blender_auto_import.sh
+++ blender_auto_import.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+# Use this to read BLENDER environment var
+source ~/.profile
+
+# if blank, something isnt working
+echo "$BLENDER"
 
 # Thanks to Claudio Sabato
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 # $@ gets replaced with args to this bash script
 # & at end will fork this command so this script isnt running all the time in the background
-$BLENDER $SCRIPT_DIR/Blank.blend --python $SCRIPT_DIR/blender_auto_import_script.py -- --auto-import $@ &
+"$BLENDER" "$SCRIPT_DIR/Blank.blend" --python "$SCRIPT_DIR/blender_auto_import_script.py" -- --auto-import "$1" &
+
+# for debugging
+#sleep 10
